717c09702083449cf2d891cea7c05038b6eb2dfb
rakjido <ipos222@naver.com>
2020-11-18 14:08:45 +0900

    add

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def get_helloworld():
     return 'hello world'
 
+def get_bye():
+    return 'bye'
+
 def test_get_helloworld():
   assert 'hello world' == get_helloworld()
+
+
+def test_get_helloworld():
+  assert 'bye' == get_bye()
 
 
 def main():
